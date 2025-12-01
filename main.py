96e88ed64c4b5de80cb32541ff67be0c9d421fc6
Jose Francisco Brothers Radilla <jfbrothers42@gmail.com>
2025-12-01 12:41:06 -0600

    Fix filter condition for 'Plant' column in load_difference

--- main.py
+++ main.py
@@ -187,7 +187,7 @@ def group_status(df, status):
 def load_diference(path):
     df = pd.read_excel(path, sheet_name="Interface_510")
     df["Plant"] = df["Plant"].astype(str)
-    df = df.loc[df["Plnt"].isin(["560","561"])] 
+    df = df.loc[df["Plant"].isin(["560","561"])] 
     df["Delta total"] = df["Delta total"].astype(str)
     df["Delta total"] = df["Delta total"].str.replace(r'\.0$', '', regex=True)
     df["Delta total"] = df["Delta total"].astype(float)
